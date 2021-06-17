197ae067a4a27559fcbdb996f082e59d89231c0b
lovobin <3327511395@qq.com>
2021-06-17 18:13:20 +0800

    feat:webpack5 content supplement

--- src/js/element.js
+++ src/js/element.js
@@ -1,9 +1,25 @@
 import '../css/style.css'
+import '../css/img.css'
+
+import scImg from '../img/test-img-loader.png'
 
 // 创建元素
 const divEl = document.createElement('div')
+const divImg = document.createElement('div')
+
+const imgE = document.createElement('img')
+imgE.src = scImg
 
 // 添加类
 divEl.className = 'title'
+divImg.className = 'body-img'
+
 divEl.innerHTML = 'Hello webpack'
+
 document.body.appendChild(divEl)
+document.body.appendChild(divImg)
+
+document.body.appendChild(imgE)
+
+const appLength = length
+console.log(appLength)
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,27 @@
 const path = require('path')
 
+// 导入插件
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+// webpack 内置插件
+const { DefinePlugin } = require('webpack')
+
 // commenjs 语法
 module.exports = {
+  /*
+   * 设置模式:
+   *   development: 开发阶段 会设置 development
+   *   production 准备打包上线的时候,设置 production
+   * 设置 source-map 建立 js 映射文件，方便调式代码和错误
+   */
+  mode: 'development',
+  devtool: 'source-map',
   entry: './src/js/index.js',
   output: {
     path: path.resolve(__dirname, './build'), // 必须是绝对路径
-    filename: 'bundle.js',
+    filename: 'js/bundle.js',
   },
 
   module: {
@@ -14,6 +30,38 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader', 'postcss-loader'],
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        type: 'asset',
+        generator: {
+          filename: 'img/[name]_[hash:6][ext]',
+        },
+        parser: {
+          dataUrlCondition: {
+            maxSize: 100 * 1024,
+          },
+        },
+      },
     ],
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new DefinePlugin({
+      BASE_URL: "'./'",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'public', // 从哪里复制
+          //  to: 'build', // 复制到哪里
+          globOptions: {
+            ignore: ['**/index.html'], // 忽略文件
+          },
+        },
+      ],
+    }),
+  ],
 }
